5459ec3966e8e3ad9fe5e5c4fb0c2ef1dd02bef4
MGCurtis <mcurtis664@gmail.com>
2016-04-19 12:47:55 +0100

    lookAt fix

--- summerGame/Assets/Scripts/EnemyShip.cs
+++ summerGame/Assets/Scripts/EnemyShip.cs
@@ -3,16 +3,16 @@ using System.Collections;
 
 public class EnemyShip : Ship {
 
-	public Transform target;
+	protected Transform target;
 
 	// Use this for initialization
 	void Start () {
-	
+		target = GameObject.FindGameObjectWithTag("Player").transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		transform.LookAt(target);
-	
+
 	}
 }
