c7bcf72657901caba6bc7102ae20d7a63ca2b248
MGCurtis <mcurtis664@gmail.com>
2016-04-19 09:15:27 +0100

    unity project made

---
+++ summerGame/Assets/Scripts/PlayerShip.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerShip : Ship {
+
+	protected void Movement () {
+		if(Input.GetKey(KeyCode.W))
+			transform.Translate(new Vector3(0,-1,0) * moveSpeed * Time.deltaTime);
+		if(Input.GetKey(KeyCode.S))
+			transform.Translate(new Vector3(0,1,0) * moveSpeed * Time.deltaTime);
+		if(Input.GetKey(KeyCode.A))
+			transform.Rotate(new Vector3(0,0,-1) * turnSpeed * Time.deltaTime);
+		if(Input.GetKey(KeyCode.D))
+			transform.Rotate(new Vector3(0,0,1) * turnSpeed * Time.deltaTime);
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Movement();
+
+		fDelay --;
+
+		if(Input.GetKey(KeyCode.Space))
+			Fire();
+	}
+}
---
+++ summerGame/Assets/Scripts/Ship.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Ship : MonoBehaviour {
+
+	public float moveSpeed = 20f;
+	public float turnSpeed = 100f;
+	public int health = 5;
+	public Object pBullet;
+	public Transform shipNose;
+	public float fDelay;
+	
+	protected void Fire () {
+		if(fDelay <= 0)
+		{
+			fDelay = 30f;
+			Instantiate(pBullet, shipNose.position, shipNose.rotation);
+		}
+	}
+	
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
