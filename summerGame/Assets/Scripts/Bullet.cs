8d12932700f74d08358e15aaf168444949d6c66c
MGCurtis <mcurtis664@gmail.com>
2016-04-19 23:42:49 +0100

    fixed shooting

---
+++ summerGame/Assets/Scripts/Bullet.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Bullet : MonoBehaviour {
+
+	private float moveSpeed = 50f;
+	public float timer = 75f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	protected void Update () {
+		transform.Translate(new Vector3(0,0,1) * moveSpeed * Time.deltaTime);
+		timer --;
+		if(timer <= 0)
+			Destroy(gameObject);
+	}
+}
---
+++ summerGame/Assets/Scripts/eBullet.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class eBullet : Bullet {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		base.Update();
+	}
+
+	void OnTriggerEnter (Collider trig)
+	{
+		if(trig.gameObject.tag == "Player")
+			Destroy(gameObject);
+		
+	}
+}
