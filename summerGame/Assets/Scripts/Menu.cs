6f9bb3c49aa75f2747bdfa0437b99e8c80cf799f
MGCurtis <mcurtis664@gmail.com>
2016-04-20 12:57:26 +0100

    title functioning

---
+++ summerGame/Assets/Scripts/Menu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI()
+	{
+		if(GUI.Button(new Rect(Screen.width*0.89f, Screen.height*0.89f, Screen.width*0.1f, Screen.height*0.1f), "Play"))
+			Application.LoadLevel("game");
+	}
+}
