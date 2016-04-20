96b5eadb0dedcaa345456aa5d7e3a6fff62d3da6
MGCurtis <mcurtis664@gmail.com>
2016-04-20 09:35:24 +0100

    powerups spawning

--- summerGame/Assets/Scripts/SceneController.cs
+++ summerGame/Assets/Scripts/SceneController.cs
@@ -8,9 +8,12 @@ public class SceneController : MonoBehaviour {
 	public float pUYLim = 30f;
 	public float pUXLim = 15f;
 	public int enemyCount = 300;
-	public int pUCount = 300;
+	public int pUCount = 720;
 
 	public GameObject enemy;
+
+	public GameObject health;
+	public GameObject gun;
 
 	// Use this for initialization
 	void Start () {
@@ -23,6 +26,7 @@ public class SceneController : MonoBehaviour {
 		pUCount --;
 
 		SpawnEnemy();
+		SpawnPowerUp();
 
 	}
 
@@ -34,5 +38,21 @@ public class SceneController : MonoBehaviour {
 			                               Random.Range(-enemyXLim, enemyXLim)), Quaternion.identity);
 		}
 
+	}
+
+	void SpawnPowerUp () {
+		if(pUCount <= 0)
+		{
+			int pUType = Random.Range(1,3);
+			pUCount = 720;
+
+			if(pUType == 1)
+				Instantiate(gun, new Vector3(Random.Range(-pUYLim,pUYLim), 0,
+				                               Random.Range(-pUXLim, pUXLim)), Quaternion.identity);
+			else if(pUType == 2)
+				Instantiate(health, new Vector3(Random.Range(-pUYLim,pUYLim), 0,
+				                               Random.Range(-pUXLim, pUXLim)), Quaternion.identity);
+
+		}
 	}
 }
